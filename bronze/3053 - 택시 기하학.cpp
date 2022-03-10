a4a2353fc7853bc763959b9cd1c2eaac5d5a25b8
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-03-10 17:46:30 +0900

    Create 3053 - 택시 기하학.cpp

---
+++ bronze/3053 - 택시 기하학.cpp
@@ -0,0 +1,13 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double r;
+	cin >> r;
+	printf("%lf\n", r * r * M_PI);
+	printf("%lf\n", r * r * 2);
+	return 0;
+}
