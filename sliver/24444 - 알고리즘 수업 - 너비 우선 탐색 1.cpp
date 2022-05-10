0f942cef0be3ea1d1ac17fb5030b65f8a4df5be8
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-05-10 10:59:00 +0900

    Create 24444 - 알고리즘 수업 - 너비 우선 탐색 1.cpp

---
+++ sliver/24444 - 알고리즘 수업 - 너비 우선 탐색 1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define SIZE 100001
+using namespace std;
+
+int N, M, V, order = 1;
+int visited[SIZE];
+vector<int> graph[SIZE];
+
+void BFS(int s)
+{
+	queue<int> q;
+	q.push(s);
+	visited[s] = order++;
+
+	while (!q.empty())
+	{
+		int v = q.front();
+		q.pop();
+
+		int len = graph[v].size();
+		for (int i = 0; i < len; i++)
+		{
+			int next = graph[v][i];
+			if (!visited[next])
+			{
+				q.push(next);
+				visited[next] = order++;
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> M >> V;
+	for (int i = 0; i < M; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	for (int i = 1; i <= N; i++)
+		sort(graph[i].begin(), graph[i].end());
+
+	BFS(V);
+	for (int i = 1; i <= N; i++)
+		cout << visited[i] << "\n";
+
+	return 0;
+}
