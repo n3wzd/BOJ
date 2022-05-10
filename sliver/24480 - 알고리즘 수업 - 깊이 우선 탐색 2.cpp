2a7c663136e7a42744924f2217a38f1d4d8867d2
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-05-10 10:58:56 +0900

    Create 24480 - 알고리즘 수업 - 깊이 우선 탐색 2.cpp

---
+++ sliver/24480 - 알고리즘 수업 - 깊이 우선 탐색 2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define SIZE 100001
+using namespace std;
+
+int N, M, V, order = 1;
+int visited[SIZE];
+vector<int> graph[SIZE];
+
+void DFS(int v)
+{
+	visited[v] = order++;
+	int len = graph[v].size();
+	for (int i = 0; i < len; i++)
+	{
+		int next = graph[v][i];
+		if (!visited[next])
+			DFS(next);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> M >> V;
+	for (int i = 0; i < M; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	for (int i = 1; i <= N; i++)
+		sort(graph[i].begin(), graph[i].end(), greater<int>());
+
+	DFS(V);
+	for (int i = 1; i <= N; i++)
+		cout << visited[i] << "\n";
+
+	return 0;
+}
