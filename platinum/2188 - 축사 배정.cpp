aff60c2bec8e9cd11f7e5ed0d63f39821e58cf9c
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-06-26 20:53:55 +0900

    Create 2188 - 축사 배정.cpp

---
+++ platinum/2188 - 축사 배정.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <memory.h>
+#define SIZE 202
+using namespace std;
+int N, M;
+vector<int> graph[SIZE];
+int matching[SIZE];
+bool visited[SIZE];
+
+bool DFS(int v) {
+	for (int i = 0; i < graph[v].size(); i++) {
+		int nv = graph[v][i];
+		if (visited[nv]) continue;
+		visited[nv] = 1;
+
+		if (matching[nv] == 0 || DFS(matching[nv])) {
+			matching[nv] = v;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int Bipartite() {
+	int mat = 0;
+	for (int v = 1; v <= N; v++) {
+		memset(visited, 0, sizeof(visited));
+		if (DFS(v)) mat++;
+	}
+	return mat;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		int len, work;
+		cin >> len;
+		while (len--) {
+			cin >> work;
+			graph[i].push_back(work);
+		}
+	}
+	cout << Bipartite();
+	return 0;
+}
